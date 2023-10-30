7a6217ee23a176c096bd4c1f39afffe01802620e
bilal patiwala <bilalpatiwala04@gmail.com>
2023-10-30 21:25:56 +0530

    changed css

--- src/Components/Right.jsx
+++ src/Components/Right.jsx
@@ -51,15 +51,15 @@ const Right = ({selectedSectionRef}) => {
             
             <div className='project-list'>
                 {data.map((project, index) => (
-                    <div key={index} class="max-w- p-6 bg-white border border-gray-200 rounded-lg shadow bg-[#1a2639] dark:border-gray-700">
+                    <div key={index} class="max-w- p-6 bg-white border border-gray-200 rounded-lg shadow bg-[#1a2639] border-gray-700">
                         <p>
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.title}</h5>
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-white">{project.title}</h5>
                         </p>
                         <ul className='point-list'>
-                            <li class="mb-3 font-normal text-[#ececec] dark:text-gray-400">{project.description}</li>
-                            <li class="mb-3 font-normal text-[#ececec] dark:text-gray-400">{project['tech-stack']}</li>
+                            <li class="mb-3 font-normal text-[#ececec] text-gray-400">{project.description}</li>
+                            <li class="mb-3 font-normal text-[#ececec] text-gray-400">{project['tech-stack']}</li>
                         </ul>
-                        <a href={project.link} target='blank' class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <a href={project.link} target='blank' class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 bg-blue-600 focus:ring-blue-800">
                             Read more
                             <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
