c1091468b0b57209acf2422aa71f5a24c213c5b4
bilal patiwala <bilalpatiwala04@gmail.com>
2023-11-01 19:41:09 +0530

    Socials

--- src/Components/Right.jsx
+++ src/Components/Right.jsx
@@ -121,7 +121,7 @@ const Right = ({selectedSectionRef}) => {
             ))}
         </div>
         
-        <div className="social" id='#Socials'>
+        <div className="social" id='Socials'>
             <p className='common-headline'>Contact, Socials</p>
             <div>
                 <p>Github</p>
